b3270a6534f49c81e79015ae3698c0e306a9ce2d
Lena Schnedlitz <lena.schnedlitz@mail.de>
2019-03-31 14:12:54 +0200

    Increase version number

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@
 
 JAR=`which jar`
 ICLS_FILE_NAME=jetbrains-color-scheme.icls
-VERSION=l0.1.0
+VERSION=1.0.0
 
 if [ -z "$JAR" ]; then
   echo "Required tools are missing or not available on the path: jar"
